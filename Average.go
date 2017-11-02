8928283ef32bb96aa15be9f563271951de38a554
vagrant <vagrant@DevOps>
2017-11-02 18:28:05 +0000

    revived all

--- Average.go
+++ Average.go
@@ -3,8 +3,8 @@ package math
 func Average(xs []float64) float64 {
 	total := float64(0)
 	for _, x := range xs {
-//		total += x
+		total += x
 	}
-//	ret := total / float64(len(xs))
+	ret := total / float64(len(xs))
 	return ret
 }
